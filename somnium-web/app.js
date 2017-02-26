c824c0d4450518b913da95276700cc4cae1551c3
Richard Harper <rharper10@msudenver.edu>
2017-02-26 14:00:26 -0700

    Add navigation to landing page

--- somnium-web/app.js
+++ somnium-web/app.js
@@ -1,10 +1,12 @@
+
 var path = require('path');
+
 var express = require('express');
 var app = express();
 
 var port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('node_modules'));
 
 app.get('/',function(req, res){    
     var file = path.join(__dirname,'views/index.html');
